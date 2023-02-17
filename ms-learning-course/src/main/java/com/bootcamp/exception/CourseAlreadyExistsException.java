13d58dc997a27cc08657ce882cf87849e7fc826d
LuizFJP <luizfjportela@gmail.com>
2023-02-17 15:04:45 -0300

    remove submodule learning-course

--- ms-learning-course/src/main/java/com/bootcamp/exception/CourseAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.exception;
-
-public class CourseAlreadyExistsException extends RuntimeException{
-  public CourseAlreadyExistsException() {
-    super("Course already exists.");
-  }
-} 
